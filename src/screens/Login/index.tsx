767356ab2dd76f74f305e47bea73ef8a868b41f6
BrenoRev <silvabreno462@gmail.com>
2022-10-11 01:18:35 -0300

    Tela inicial

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,18 +1,56 @@
-import React from 'react';
-import {View, Text, Button, StyleSheet} from 'react-native';
+import React, { useState } from 'react';
+import { SafeAreaView, Image, TextInput, Text, Alert, TouchableOpacity, View, ScrollView } from 'react-native';
 import style from './styles';
+import imageLogo from '../../assets/img-logo.png';
 
-const metodo = () => {
-  console.log('teste');
-};
 
 const Login = () => {
-  return (
-    <View style={style.container}>
-      <Text style={style.text}>Hello World</Text>
-      <Button title="Click Me" onPress={() => metodo()} />
-    </View>
-  );
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = () => {
+        Alert.alert('Seja bem vindo, ' + email);
+        console.log(email, password);
+        if(email.length < 6) {
+            Alert.alert('Email inválido');
+        }
+
+        if(password.length < 6) {
+            Alert.alert('Senha inválida');
+        }
+    } 
+    return (
+        <ScrollView style={style.scroll}>
+        <SafeAreaView style={style.container}>
+            <Image source={imageLogo} style={style.image} resizeMode="contain"/>
+            <View style={style.content}>
+                <TextInput 
+                    placeholder="Email"
+                    style={style.input}
+                    value={email}
+                    onChangeText={(text) => setEmail(text)}
+                    />
+                <TextInput 
+                    placeholder="Senha" 
+                    secureTextEntry={true}
+                    value={password}
+                    onChangeText={(text) => setPassword(text)}
+                    style={style.input}/>
+                <TouchableOpacity style={style.button}>
+                    <Text 
+                        onPress={() => handleLogin()}
+                        style={style.textButton}> 
+                            Entrar 
+                    </Text>
+                </TouchableOpacity>
+                <Text 
+                    style={style.text}> 
+                        Esqueci Minha Senha 
+                </Text>
+            </View>
+        </SafeAreaView>
+        </ScrollView>
+    );
 };
 
 
--- src/screens/Login/styles.tsx
+++ src/screens/Login/styles.tsx
@@ -1,14 +1,59 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Dimensions } from 'react-native';
+
+const { height } = Dimensions.get('window');
 
 export default StyleSheet.create({
     container: {
       flex: 1,
+      height: height,
+      width: "100%",
       justifyContent: 'center',
       alignItems: 'center',
-      backgroundColor: '#616161',
+      backgroundColor: '#ffffff',
     },
   
+    scroll: {
+        height: "100%",
+    },
+
+    image: {
+        height: "10%",
+    },
+
+    content: {
+        width: "100%",
+        marginTop: 50,
+        padding: 16,
+    },
+
+    input: {
+        width: "100%",
+        height: 60,
+        borderWidth: 1,
+        marginTop: 18,
+        borderRadius: 10,
+        borderColor: "#BDBDBD",
+        padding: 8,
+        fontWeight: "bold",
+    },
+
+    textButton: {
+        color: "#ffffff",
+        fontWeight: "bold",
+    },
+
     text: {
-      color: '#ffffff'
+        width: "100%",
+        marginTop: 12,
+        textAlign: "right",
+    },
+
+    button: {
+        height: 45,
+        marginTop: 30,
+        backgroundColor: "#027890",
+        alignItems: "center",
+        justifyContent: "center",
+        borderRadius: 10, 
     }
   });
